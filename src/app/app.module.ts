644fd7000e02b6f6de92326718454ad9505fbc48
MangeshG <Mangesh.g@intelgain.com>
2018-01-11 22:22:30 +0530

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {ReactiveFormsModule} from '@angular/forms'
 
 import { AppComponent } from './app.component';
+import { ObservableDemoComponent } from './observable-demo/observable-demo.component';
+import { FormArray } from '@angular/forms/src/model';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ObservableDemoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
